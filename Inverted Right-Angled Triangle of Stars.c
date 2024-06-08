a9e83d2f73b195cb3ab4124d6fe427420b12c324
sridhar venkatesh <shreekarpasupuleti@gmail.com>
2024-06-08 21:15:57 +0530

    opt Inverted RAT

--- Inverted Right-Angled Triangle of Stars.c
+++ Inverted Right-Angled Triangle of Stars.c
@@ -23,5 +23,22 @@ int main(){
         printf("\n");
     }
 
+
+    for (int lineNum = 1 ; lineNum <= nLines; lineNum++) {
+
+        /*
+        1 - 5+1 -1 
+        2 - 5+1 -2 
+        ....
+
+        lineNum -  (nLines+1 - lineNum)
+        
+         */
+        for (int count = nLines-lineNum+1; count >= 1; count--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
     return 0;
 }
