8f501208393992912e27e826c31536886568d103
Shreekar Pasupuleti <shreekarpasupuleti@gmail.com>
2024-06-27 22:22:10 +0530

    Update sum of the digits.c

--- MATH/sum of the digits.c
+++ MATH/sum of the digits.c
@@ -1,15 +1,15 @@
 /*7.	Sum of Digits
 	•	Write a program to find the sum of the digits of a given number.*/
-    #include <stdio.h>
-    int main (){
-        int a,r,sum=0;
-        printf("Enter a number :");
-        scanf("%d",&a);
-        while(a>0){
-            r=a%10;
-            sum = sum +r;
-            a=a/10;
+#include <stdio.h>
+int main (){
+int a,r,sum=0;
+printf("Enter a number :");
+scanf("%d",&a);
+while(a>0){
+	r=a%10;
+        sum = sum +r;
+        a=a/10;
         }
-        printf("%d",sum);
-        return 0;
-    }
+printf("%d",sum);
+return 0;
+}
