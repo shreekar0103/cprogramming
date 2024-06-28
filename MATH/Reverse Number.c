5cd04db5bd1ef35b515e271e75d805847b947833
sridhar venkatesh <shreekarpasupuleti@gmail.com>
2024-06-28 23:21:14 +0530

    Reverse number

---
+++ MATH/Reverse Number.c
@@ -0,0 +1,26 @@
+/*8.	Reverse a Number
+	•	Write a program to reverse the digits of a given number.*/
+#include<stdio.h>
+int main(){
+    int n,r;
+    int f = 0;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    if ( n == 0 ){
+        printf("0");
+        return 0;
+    }
+    while(n>0){
+        r = n%10;
+        if(f==0){
+            if (r!=0){
+                f=1;
+            }
+        }
+        if(f==1){
+            printf("%d",r);
+        }
+        n=n/10;
+    }   
+    return 0;
+}
